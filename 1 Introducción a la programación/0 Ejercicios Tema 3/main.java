703eb10172d653a513fdd783ec0e5f6a719ad577
iagovar <iagovar@outlook.com>
2022-04-15 13:09:09 +0200

    Uploading code for asking questions in Discord

--- 1 Introducción a la programación/0 Ejercicios Tema 3/main.java
+++ 1 Introducción a la programación/0 Ejercicios Tema 3/main.java
@@ -5,15 +5,41 @@ public class Main {
     return resultado;
   }
 
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     /*
     Primera parte.
     Crear una función con tres parámetros que sean números que se suman entre sí.
     Llamar a la función en el main y darle valores..
     */
     float resultado = sumArbitraryParams(20,20,20);
-    System.out.println("El resultado de la suma de parámetros es:\n");
+    System.out.println("El resultado de la suma de parámetros es:");
     System.out.println(resultado);
 
+    /*
+    Segunda parte.
+
+    Crear una clase coche con una variable que contenga el número de puertas,
+    y una función/método que permita agregar puertas a la instancia.
+
+    Después crear un nuevo objeto "miCoche" en el main y añadirle una puerta.
+
+    A continuación, mostrar en consola el número de puertas del coche.
+    */
+
+    Coche miCoche = new Coche();
+    miCoche.incrementarPuertas(2);
+    System.out.println("El número de puertas es" + miCoche.puertasCoche);
+
   }
+}
+
+
+public class Coche {
+
+  int puertasCoche = 5;
+
+  public static void incrementarPuertas(int masPuertas) {
+    this.puertasCoche += masPuertas;
+  }
+
 }
