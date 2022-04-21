7a57a10d40a7d38a352d0cbb7c626f7c33417b81
iagovar <iagovar@outlook.com>
2022-04-21 12:11:54 +0200

    Control flow exercise 1 for python

---
+++ 2 Curso de Python/4 Control de flujo/1 Ejercicio 1/main.py
@@ -0,0 +1,6 @@
+edad = int(input("Por favor, introduce tu edad: "))
+
+if edad >= 18:
+	print(f"Puesto que tienes {edad}, eres mayor de edad.")
+else:
+	print(f"Con {edad} años aún eres menor de edad.")
