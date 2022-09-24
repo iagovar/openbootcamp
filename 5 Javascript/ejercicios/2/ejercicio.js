45aa26ec0e362d076dceb0a7b9a2ae2963914551
iagovar <iagovar@outlook.com>
2022-09-24 14:28:31 +0200

    Segundo ejercicio Js

---
+++ 5 Javascript/ejercicios/2/ejercicio.js
@@ -0,0 +1,12 @@
+const miNombre = "Iago";
+const miEdad = "34";
+const eresDesarollador = false;
+const miNacimiento = new Date(1987, 11, 27);
+const miLibroFavorito = {
+    titulo: "Abundancia Roja",
+    autor: "Francis Spufford",
+    fecha: new Date(2011, 9, 17),
+    url: "https://amzn.to/3C5WMUP"
+}
+
+console.log(miLibroFavorito);
