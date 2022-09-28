9fd1c62ab462dbd470c539f2272d875fd4d08670
iagovar <iagovar@outlook.com>
2022-09-28 23:25:55 +0200

    Termianos ejercicios numeros JS

---
+++ 5 Javascript/ejercicios/5 - Numeros/ejercicio.js
@@ -0,0 +1,17 @@
+let alturaCentimetros = 180;
+let alturaMetros = alturaCentimetros / 100;
+let pesoKilogramos = 70.46;
+let alturaMetrosRedondeada = alturaMetros.toFixed(2);
+
+let pesoKilogramosBaja = pesoKilogramos.toString() 
+
+let maximoValor = (Number.MAX_VALUE == Number.MAX_VALUE + 1) ? true : false;
+
+console.log(maximoValor);
+
+
+
+
+/*
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+*/
